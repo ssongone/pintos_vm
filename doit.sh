5cc486685780ccccf4f38b68d857c1486632b89c
Hojip-Kim <rlaghwlq12@naver.com>
2023-10-04 10:02:15 +0000

    hojip

--- doit.sh
+++ doit.sh
@@ -1,9 +1,9 @@
-source ./activate
-make clean
 cd userprog
-make check
-
-# cd threads
-# make
-# cd build
-# pintos -- -q run priority-donate-one
+make clean
+make
+cd build
+source ../../activate
+# pintos --gdb -- run priority-donate-chain
+# pintos -- run priority-donate-chain
+pintos --gdb --fs-disk=10 -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
+# pintos --fs-disk=10 -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
