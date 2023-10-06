296811e13449a220b2e690bc8b411eb5315c9f47
Hojip-Kim <rlaghwlq12@naver.com>
2023-10-06 12:26:08 +0000

    Merge branch 'master' of https://github.com/ssongone/pintos into hojipKim2

--- doit.sh
+++ doit.sh
@@ -12,4 +12,9 @@ source ../../activate
 #pintos -fs-disk=10 -p tests/userprog/open-boundary:open-boundary -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run open-boundary
 # --fs-disk=10 -p tests/userprog/open-twice:open-twice -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run open-twice
 
-pintos --gdb --fs-disk=10 -p tests/userprog/close-twice:close-twice -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run close-twice
+# pintos --fs-disk=10 -p tests/userprog/close-twice:close-twice -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run close-twice
+# pintos --fs-disk=10 -p tests/userprog/read-normal:read-normal -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run read-normal
+
+pintos --fs-disk=10 -p tests/userprog/read-bad-ptr:read-bad-ptr -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run read-bad-ptr
+# --fs-disk=10 -p tests/userprog/read-boundary:read-boundary -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run read-boundary
+# --fs-disk=10 -p tests/userprog/read-zero:read-zero -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run read-zero
--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -8,14 +8,18 @@ void syscall_init (void);
 
 void check_addr(const uint64_t *addr);
 struct file *find_file_by_Fd(int fd);
+int add_file_to_fdt(struct file *file);
 
 void exit(int status);
+void call_close(int fd);
 void call_exit(struct thread* curr, uint64_t status);
 int call_write(const void *buffer, unsigned size);
 bool call_create (const char *file, unsigned initial_size);
 void call_halt(void);
 int call_open(const char *file);
-int add_file_to_fdt(struct file *file);
+int call_read(int fd, void *buffer, unsigned size);
+int call_filesize(int fd);
+
 
 
 #endif /* userprog/syscall.h */
